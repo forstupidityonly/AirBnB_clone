fa10c103b04b912440cc732a278aec3283ab0175
Corbin <forstupidityonly@gmail.com>
2021-02-12 18:04:12 -0600

    do_create and do show issues

--- console.py
+++ console.py
@@ -21,17 +21,26 @@ class HBNBCommand(cmd.Cmd):
         pass
         return True
 
-    def do_create(self, args):
+    def do_create(self, *args):
         """Creates a new instance of BaseModel, and saves it"""
         if args == "" or args is None:
             print("** class name missing **")
-        elif args not in models.class_dict:
+        elif args[0] not in models.class_dict:
             print("** class doesn't exist **")
         else:
-            func = models.class_dict[args]
+            func = models.class_dict[args[0]]
             temp = func()
             temp.save()
             print(temp.id)
+
+    def do_show(self, *args):
+        """Prints the string representation of an instance based on the class name and id"""
+        if args == "" or args is None:
+            print("** class name missing **")
+        elif args[0] not in models.class_dict:
+            print("** class doesn't exits **")
+        else:
+            pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
