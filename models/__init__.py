5a5e7dc1ae81854e7e4a019343c79bae76447e0b
Selidex <justindparnell@yahoo.com>
2021-02-15 20:19:23 +0000

    Updated class dict

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,15 @@
 from models.engine import file_storage
 import models.base_model
 import models.user
+import models.state
+import models.city
+import models.amenity
+import models.place
+import models.review
 
-class_dict = {"BaseModel": base_model.BaseModel, "User": user.User}
+
+class_dict = {"BaseModel": base_model.BaseModel, "User": user.User,
+              "Place": place.Place, "City": city.City, "State": state.State,
+              "Amenity": amenity.Amenity, "Review": review.Review}
 storage = file_storage.FileStorage()
 storage.reload()
