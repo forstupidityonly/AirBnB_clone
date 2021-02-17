38982280e408544a3cb05911ccb3ab4c2b0a9e7e
Selidex <justindparnell@yahoo.com>
2021-02-17 22:03:54 +0000

    Updating user and user tests

--- models/user.py
+++ models/user.py
@@ -6,8 +6,11 @@ import models
 class User(models.base_model.BaseModel):
     """Stores AirBnB user information"""
     def __init__(self, *args, **kwargs):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-        super().__init__()
+        if not kwargs:
+            self.email = ""
+            self.password = ""
+            self.first_name = ""
+            self.last_name = ""
+            super().__init__()
+        else:
+            super().__init__(**kwargs)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,3 +11,14 @@ class Test_User(unittest.TestCase):
         """testing attributes"""
         b1 = User()
         self.assertEqual(b1.first_name, "")
+        self.assertEqual(b1.last_name, "")
+        self.assertEqual(b1.email, "")
+        self.assertEqual(b1.password, "")
+        test_dict = {'created_at': '2021-02-17T02:16:39.282299',
+                     'updated_at': '2021-02-17T02:16:39.282323',
+                     'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632',
+                     'email': 'bob_saget@gmail.com', 'password': 'top_secret',
+                     'first_name': 'Bob', 'last_name': 'Saget',
+                     '__class__': 'User'}
+        b2 = User(**test_dict)
+        self.assertDictEqual(b2.to_dict(), test_dict)
