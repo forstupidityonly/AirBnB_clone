691b9cbcc7b24db4a8d69efe2442e825d7e082a8
Selidex <justindparnell@yahoo.com>
2021-02-17 22:27:51 +0000

    Updating tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,19 +10,12 @@ class Test_Base_model(unittest.TestCase):
     """base model testing"""
     def test_attributes(self):
         """testing attributes"""
-        b1 = BaseModel()
-        self.assertEqual(b1.id, b1.id)
-        self.assertEqual(b1.created_at, b1.created_at)
-        self.assertEqual(b1.updated_at, b1.updated_at)
-        self.assertDictEqual(b1.to_dict(), b1.to_dict())
-        self.assertEqual(str(b1), str(b1))
-
         test_dict = {'created_at': '2021-02-17T02:16:39.282299',
                      'updated_at': '2021-02-17T02:16:39.282323',
-                     'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632'}
-
+                     'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632',
+                     '__class__': 'BaseModel'}
         b2 = BaseModel(**test_dict)
-
+        self.assertDictEqual(b2.to_dict(), test_dict)
         self.assertEqual(b2.id, '8bdb1808-7f1a-4f06-bb0c-324c46f9c632')
         self.assertEqual(b2.created_at, datetime.datetime(2021, 2, 17, 2, 16,
                                                           39, 282299))
@@ -31,4 +24,7 @@ class Test_Base_model(unittest.TestCase):
         self.assertEqual(str(b2), str(b2))
         b2.save()
         self.assertEqual(b2.updated_at, b2.updated_at)
-        self.assertEqual(b1.__class__, type(b1))
+        self.assertEqual(b2.__class__, BaseModel)
+
+        b1 = BaseModel()
+        self.assertEqual(type(b1), BaseModel)
