aa0ac1f42fbce430cc49acb1191893213b03c94a
Corbin <forstupidityonly@gmail.com>
2021-02-16 20:23:18 -0600

    base model tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,7 +3,7 @@
 import unittest
 import json
 from models.base_model import BaseModel
-
+import datetime
 
 class Test_Base_model(unittest.TestCase):
     """base model testing"""
@@ -14,3 +14,18 @@ class Test_Base_model(unittest.TestCase):
         self.assertEqual(b1.created_at, b1.created_at)
         self.assertEqual(b1.updated_at, b1.updated_at)
         self.assertDictEqual(b1.to_dict(), b1.to_dict())
+        self.assertEqual(str(b1), str(b1))
+
+        test_dict = {'created_at': '2021-02-17T02:16:39.282299',
+        'updated_at': '2021-02-17T02:16:39.282323',
+        'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632'}
+
+        b2 = BaseModel(**test_dict)
+
+        self.assertEqual(b2.id, '8bdb1808-7f1a-4f06-bb0c-324c46f9c632')
+        self.assertEqual(b2.created_at, datetime.datetime(2021, 2, 17, 2, 16, 39, 282299))
+        self.assertEqual(b2.updated_at, datetime.datetime(2021, 2, 17, 2, 16, 39, 282323))
+        self.assertEqual(str(b2), str(b2))
+        b2.save()
+        self.assertEqual(b2.updated_at, b2.updated_at)
+        self.assertEqual(b1.__class__, type(b1))
