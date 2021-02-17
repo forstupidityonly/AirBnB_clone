d8bfbb4e63e648cac578a203753361a426ac0d1e
Corbin <forstupidityonly@gmail.com>
2021-02-16 20:01:33 -0600

    more base model tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,3 +11,6 @@ class Test_Base_model(unittest.TestCase):
         """testing attributes"""
         b1 = BaseModel()
         self.assertEqual(b1.id, b1.id)
+        self.assertEqual(b1.created_at, b1.created_at)
+        self.assertEqual(b1.updated_at, b1.updated_at)
+        self.assertDictEqual(b1.to_dict(), b1.to_dict())
