105d1423bf9f8a23cc7fa05d09ff8afb2f5c9afc
Selidex <justindparnell@yahoo.com>
2021-02-17 22:15:08 +0000

    pep8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@ import json
 from models.base_model import BaseModel
 import datetime
 
+
 class Test_Base_model(unittest.TestCase):
     """base model testing"""
     def test_attributes(self):
@@ -17,14 +18,16 @@ class Test_Base_model(unittest.TestCase):
         self.assertEqual(str(b1), str(b1))
 
         test_dict = {'created_at': '2021-02-17T02:16:39.282299',
-        'updated_at': '2021-02-17T02:16:39.282323',
-        'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632'}
+                     'updated_at': '2021-02-17T02:16:39.282323',
+                     'id': '8bdb1808-7f1a-4f06-bb0c-324c46f9c632'}
 
         b2 = BaseModel(**test_dict)
 
         self.assertEqual(b2.id, '8bdb1808-7f1a-4f06-bb0c-324c46f9c632')
-        self.assertEqual(b2.created_at, datetime.datetime(2021, 2, 17, 2, 16, 39, 282299))
-        self.assertEqual(b2.updated_at, datetime.datetime(2021, 2, 17, 2, 16, 39, 282323))
+        self.assertEqual(b2.created_at, datetime.datetime(2021, 2, 17, 2, 16,
+                                                          39, 282299))
+        self.assertEqual(b2.updated_at, datetime.datetime(2021, 2, 17, 2, 16,
+                                                          39, 282323))
         self.assertEqual(str(b2), str(b2))
         b2.save()
         self.assertEqual(b2.updated_at, b2.updated_at)
