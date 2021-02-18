5bb463f9bb0dc9ce0feaa6b53d15a7cad0abeb26
Selidex <justindparnell@yahoo.com>
2021-02-18 01:00:32 +0000

    Testing something

--- models/state.py
+++ models/state.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+B#!/usr/bin/python3
 """This is the State class"""
 import models
 
@@ -6,5 +6,8 @@ import models
 class State(models.base_model.BaseModel):
     """Stores AirBnB user information"""
     def __init__(self, *args, **kwargs):
-        self.name = ""
-        super().__init__()
+        if kwargs is not None and kwargs != {}:
+            super().__init__(**kwargs)
+        else:
+            self.name = ""
+            super().__init__()
