7221de62c74d03df74bef877f7e2d98118b7f4b8
Corbin <forstupidityonly@gmail.com>
2021-02-18 12:08:23 -0600

    setUp

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,14 +8,17 @@ import datetime
 
 class Test_Base_model(unittest.TestCase):
     """base model testing"""
-    def test_attributes(self):
-        """testing attributes"""
-        test_dict = {"id": "72db1a16-1147-4c12-a72b-a91db820f1a6",
+    def setUp(self):
+        """Runs before each function"""
+        self.test_dict = {"id": "72db1a16-1147-4c12-a72b-a91db820f1a6",
         "created_at": "2021-02-18T11:30:32.494714", 
         "updated_at": "2021-02-18T11:30:32.494714"}
-        b1 = BaseModel(**test_dict)
+
+    def test_attributes(self):
+        """testing attributes"""
+        b1 = BaseModel(**self.test_dict)
         self.assertIsInstance(b1, BaseModel)
-        self.assertEqual(b1.id, test_dict["id"])
+        self.assertEqual(b1.id, self.test_dict["id"])
         self.assertIsInstance(b1.created_at, datetime.datetime)
         self.assertIsInstance(b1.updated_at, datetime.datetime)
         self.assertEqual(b1.created_at, b1.updated_at)
